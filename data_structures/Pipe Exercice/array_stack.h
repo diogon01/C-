a05356c5d8522e1357d238e59c64160c3bd0787b
root <root@agro-defender.agro42.com>
2018-03-14 10:15:48 -0300

    finish stack exercise

--- data_structures/Pipe Exercice/array_stack.h
+++ data_structures/Pipe Exercice/array_stack.h
@@ -34,6 +34,7 @@ public:
 private:
     T* contents;
     int top_;
+    std::size_t size_;
     std::size_t max_size_;
 
     const static auto DEFAULT_SIZE = 10u;
@@ -43,30 +44,85 @@ private:
 
 #endif
 
-
+/** New Construction **/
 template<typename T>
-
-bool structures::ArrayStack<T>::full(){
-
-    return (top_ == std::size_t max_size() -1);
-
-}
-
-template<typename T>
-
-bool structures::ArrayStack<T>::empty(){
-    return  top_ == -1;
-}
-
-template<typename T>
-
-bool structures::ArrayStack<T>::empty(){
-    return  top_ == -1;
-}
-
-template<typename T>
-
-void structures::ArrayStack::push(top_){
+structures::ArrayStack<T>::ArrayStack(std::size_t max){
     
+    size_ = 0;
+    max_size_ = max;
+    contents = new T[max_size_];
 }
+
+/** Destruction **/
+template<typename T>
+structures::ArrayStack<T>::~ArrayStack(){
+    delete[] contents;
+}
+
+/* Push  on Stack */
+template<typename T> 
+void structures::ArrayStack<T>::push(const T& data){
+    if(full())
+        throw std::out_of_range("Stack is full");
+    else
+        contents[size_++] = data;
+}
+
+/* Remove the Stack */
+template<typename T>
+T structures::ArrayStack<T>::pop(){
+    if(empty()){
+        throw std::out_of_range("Stack is empty");
+    } else {
+        
+        auto temp = contents[0];
+        for (int i = 0; i < size_; i++)
+            contents[i] = contents[i++];
+        size_--;
+        return temp;
+    }
+}
+
+/* remove the last data on stack */
+template<typename T>
+ T& structures::ArrayStack<T>::top(){
+    if(empty())
+        throw std::out_of_range("Stack is empty");
+    return contents[size_ -1];
+}
+
+
+/* Clear Stack */
+template<typename T>
+void structures::ArrayStack<T>::clear(){
+    size_ = 0;
+}
+
+/* Get the stack lenght */
+template<typename T>
+std::size_t structures::ArrayStack<T>::size(){
+    return size_;
+}
+
+/* Get the maximum stack size */
+template<typename T>
+std::size_t structures::ArrayStack<T>::max_size(){
+    return max_size_;
+}
+
+/* Verify the stack is empty */
+
+template<typename T>
+bool structures::ArrayStack<T>::empty(){
+    return  top_ == -1;
+}
+
+
+/* Veriy the stack is full */
+template<typename T>
+bool structures::ArrayStack<T>::full(){
+    return size_ == max_size_;
+}
+
+
 
